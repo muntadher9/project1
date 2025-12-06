e09d8e173a61ae51fc9436d0397e5ef2a97420c0
User <user@example.com>
2025-12-06 23:57:58 +0300

    Deploy more Iraqi dialect copy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0c4c0085970351b07105c6fa71a09396
 "demos/web/index.html": "161446a461c37b0b224452f8a9bf11a3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "f1f7830e39dabf01f2a306c212c04e1a",
+"flutter_bootstrap.js": "587e4cd17a1a74894f091b85df3c39b9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

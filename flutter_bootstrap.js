3ae9749fee779298704e4e853a2d7cc126216ce3
User <user@example.com>
2025-12-07 20:18:55 +0300

    Reduce text, add icon summaries

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4130679981"
+    serviceWorkerVersion: "151630664"
   }
 });
